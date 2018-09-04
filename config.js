3435d2c834ae80496dcef67648c2c164eca16172
Kim Wonjin <onejean81@gmail.com>
2018-09-04 14:00:55 +0900

    add certificate files and config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// define('AUTH_KEY',         '17sUJPRglDNZAJ_t@_D|<-xYR_q/oR%J.v&ffYyPT)`Y]:KruIDlHWj0>6a|fjx)');
+// define('SECURE_AUTH_KEY',  'OrsywKl1JHO7iJ*/5t$e-2n<|qX1A9S:JQ d4_5Nn5?qW7E*iGmznhm~-6wQePR;');
+// define('LOGGED_IN_KEY',    'u+-UxX+sMVytv[wz;^m`BU2^>Ilyg+j 12:(sV-?b?ed0;pe``kKDR +TSaICu8Y');
+// define('NONCE_KEY',        'yPMsG(9q5wN+#7{yz@J;+w+|dA4VBw]=J28YsC+l HuB&Cl_&]Knq<z uSMg=#4p');
+// define('AUTH_SALT',        'AR3|VEO{b,$h#TAJlk,+|%Y1E2X7RFU=oP|Qkb3hNl[Ln&r$QOyL5-zF[f^9?lY8');
+// define('SECURE_AUTH_SALT', 'QP-,)Mp=^<2auh4GgPyEa|U/A|z{,h2S+t|rnN~<SJ}j~XW3(T_qME8ZlZZ!)V5a');
+// define('LOGGED_IN_SALT',   'HGm3#SK$Sz5,++&K-P[lcUZ#0-1K~CT:$|ihZ*_c#n$[td[lFItcJ!gWn:T!QorO');
+// define('NONCE_SALT',       '5%%8C+;0-@?(T(HlC6o&{JV[U/<mwP8/lL+pVSeA+|wlFVir}w!<Hd{`-O)-L:+e');
+
+module.exports = {
+    // 'secretKey': '12345-67890-09876-54321',
+    'secretKey': '17sUJPRglDNZAJ_t@_D|<-xYR_q/oR%J.v&ffYyPT)`Y]:KruIDlHWj0>6a|fjx)',
+    'mongoUrl' : 'mongodb://localhost:27017/vntcDb'
+}
