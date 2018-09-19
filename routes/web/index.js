b61a41bca29bf3692cf8882835015350450660d4
Kim Wonjin <onejean81@gmail.com>
2018-09-19 14:19:30 +0900

    Arrange router in index.js

---
+++ routes/web/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+//var authRouter = require('./authRouter');
+var memberRouter = require('./memberRouter');
+//var membersRouter = require('./membersRouter');
+var router = express.Router();
+
+//router.use('/auth', authRouter);
+router.use('/member', memberRouter);
+//router.use('/members', membersRouter);
+
+module.exports = router;
